7e7b06c448b2c9487c7f9d0925ae72358f38a3a8
Fabio Moretto <Fabio Moretto@DESKTOP-UFQVF6N>
2019-03-20 12:13:54 +0100

    Aggiunto costruttore e metodo add alla classe Libretto.

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,18 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+import java.util.*;
+
 public class Libretto {
+	
+	private List <Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList <Voto>();
+	}
+	
+	public void add(Voto v) {
+		
+	}
 
 }
